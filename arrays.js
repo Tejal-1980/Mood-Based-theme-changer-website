3062bd577e2952e2ff8775ab774593241fa56bdf
Tejal <217589681+Tejal-1980@users.noreply.github.com>
2026-04-29 15:23:21 +0530

    Removed unnecessary files

--- arrays.js
+++
@@ -1,6 +0,0 @@
-// let marks =[ 90 , 89 ,45 ,77 , 90 , 99 , " ww" ];
-// for(let idx=0 ; idx < marks.length ; idx++){
-// console.log( marks[ idx]);
-// }
-//   marks.pop();
-//  console.log(marks);
