860ca1fb51b0f32e585e6c667f6cb73d46678084
Tejal <217589681+Tejal-1980@users.noreply.github.com>
2026-04-29 15:20:42 +0530

    initial Tic Tac toe project

---
+++ arrays.js
@@ -0,0 +1,6 @@
+// let marks =[ 90 , 89 ,45 ,77 , 90 , 99 , " ww" ];
+// for(let idx=0 ; idx < marks.length ; idx++){
+// console.log( marks[ idx]);
+// }
+//   marks.pop();
+//  console.log(marks);
