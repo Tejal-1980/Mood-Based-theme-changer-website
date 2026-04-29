2c1fa156748ee2aed473b9b79c3a4342fe18ecdd
Tejal <217589681+Tejal-1980@users.noreply.github.com>
2026-04-29 15:14:02 +0530

    Removed unnecessary files

--- fetchAPI.js
+++
@@ -1,9 +0,0 @@
-const URL = "https://official-joke-api.appspot.com/random_joke";
-
-const getfacts = async() =>{
-  console.log("Fetching Data....");
-let response = await fetch(URL); 
-console.log(response);
-let data=await response.json();
-console.log(data[1].text);
-};
